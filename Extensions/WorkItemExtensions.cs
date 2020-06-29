0a3004e71066eaf41099d6d84a4ab30d6c58d074
Holzapfel <dholzapfel@a-w.com>
2020-06-29 12:28:41 +0200

    Small refactoring for property reading
    Added Test Plan, Test Suite, Shared Parameter, Shared Steps as new work item type, but without matching classes (prevent of exception/crash for our DevOps Project)

--- Extensions/WorkItemExtensions.cs
+++ Extensions/WorkItemExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using AzureDevOpsCustomObjects.Attributes;
 using AzureDevOpsCustomObjects.WorkItems;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
@@ -11,50 +12,49 @@ namespace AzureDevOpsCustomObjects.Extensions
         {
             var azureDevOpsWorkItem = InstantiateWorkItem(workItem.Fields["System.WorkItemType"].ToString());
 
-            if (workItem.Id.HasValue)
+            if (azureDevOpsWorkItem != null)
             {
-                azureDevOpsWorkItem.Id = workItem.Id.Value;
-            }
-
-            foreach (var field in workItem.Fields)
-            {
-                var isFieldAssigned = false;
-                // assign field values to properties
-                foreach (var property in azureDevOpsWorkItem.GetType().GetProperties())
+                if (workItem.Id.HasValue)
                 {
-                    var fieldPath = property.GetFieldPath();
-                    if ("/fields/" + field.Key != fieldPath)
-                        continue;
+                    azureDevOpsWorkItem.Id = workItem.Id.Value;
+                }
 
-                    if (!property.PropertyType.IsEnum)
+                foreach (var field in workItem.Fields)
+                {
+                    var isFieldAssigned = false;
+                    // assign field values to properties
+                    var property = azureDevOpsWorkItem.GetType().GetProperties().Where(p => p.GetFieldPath() == "/fields/" + field.Key).SingleOrDefault();
+                    if (property != null)
                     {
-                        property.SetValue(azureDevOpsWorkItem, field.Value);
-                        isFieldAssigned = true;
-                    }
-                    else
-                    {
-                        var enumValues = Enum.GetValues(property.PropertyType);
-                        foreach (var enumValue in enumValues)
+
+                        if (!property.PropertyType.IsEnum)
                         {
-                            var typedEnumValue = Enum.Parse(property.PropertyType, enumValue.ToString()) as Enum;
-
-                            var description = typedEnumValue.GetDescription();
-
-                            if (field.Value.ToString() != description)
-                                continue;
-
-                            property.SetValue(azureDevOpsWorkItem, typedEnumValue);
+                            property.SetValue(azureDevOpsWorkItem, field.Value);
                             isFieldAssigned = true;
-                            break;
+                        }
+                        else
+                        {
+                            var enumValues = Enum.GetValues(property.PropertyType);
+                            foreach (var enumValue in enumValues)
+                            {
+                                var typedEnumValue = Enum.Parse(property.PropertyType, enumValue.ToString()) as Enum;
+
+                                var description = typedEnumValue.GetDescription();
+
+                                if (field.Value.ToString() != description)
+                                    continue;
+
+                                property.SetValue(azureDevOpsWorkItem, typedEnumValue);
+                                isFieldAssigned = true;
+                                break;
+                            }
                         }
                     }
 
-                    break;
-                }
-
-                if (!isFieldAssigned)
-                {
-                    azureDevOpsWorkItem.AddOrReplace("/fields/" + field.Key, field.Value);
+                    if (!isFieldAssigned)
+                    {
+                        azureDevOpsWorkItem.AddOrReplace("/fields/" + field.Key, field.Value);
+                    }
                 }
             }
 
@@ -87,6 +87,12 @@ namespace AzureDevOpsCustomObjects.Extensions
                     break;
                 case "Test Case":
                     azureDevOpsWorkItem = new AzureDevOpsTestCase();
+                    break;
+                case "Test Plan":
+                case "Test Suite":
+                case "Shared Parameter":
+                case "Shared Steps":
+                    azureDevOpsWorkItem = null;
                     break;
                 default:
                     throw new Exception("Unknown Work Item Type");
